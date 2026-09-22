a0da70caa8d5e01b22f57755cea22870ab18097f
林雾烛 <linwuzhu123321@163.com>
2026-09-22 12:54:19 +0800

    26.9.22.1

--- 定义一个带一个参数的函数.c
+++ 定义一个带一个参数的函数.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 void pound(int n);
+//是函数原型也是函数声明
+//声明参数就创建了被称为形式参数的变量,简称形参,函数调用传递的值为实际参数,简称实参
+//我们可以说形参是变量,实参是函数调用的提供的值
+//void pound(int n)
+//pound(times)就是把times的值赋给n,形参就是n,实参就是times
+
 int main(void)
 {
     int times = 5;
@@ -8,15 +14,16 @@ int main(void)
 
     pound(times);
     pound(ch);
+    //会自动将字符转换成整数类型
     pound(f);
 
     return 0;
 }
-
+//变量名为函数私有,意思是在不同的函数中可以使用相同的函数名;
 
 void pound(int n)
 {
     while (n-- > 0)
-        printf("#");
+    printf("#");
     printf("\n");
 }
